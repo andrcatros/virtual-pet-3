507655f012317322c7d5b239299530b998f0f244
James Woodhead <james_woodhead95@hotmail.co.uk>
2020-11-05 21:04:39 +0000

    added a walk functionality to the virtual pet, then set a limit on fitness. Refactored code to ensure it made more sense from an outside point of view

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,5 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -10,5 +12,13 @@ Pet.prototype.growUp = function () {
     this.hunger += 5;
     this.fitness -= 3;
 };
+
+Pet.prototype.walk = function () {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    }
+}
 
 module.exports = Pet;
