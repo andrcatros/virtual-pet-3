99fe36cf92adef79459992a0edb35fa76006d278
James Woodhead <james_woodhead95@hotmail.co.uk>
2020-11-05 20:30:49 +0000

    added tests to ensure that as virtual pet grew up, its fitness decreased and its hunger increased, then implemented code to make the tests pass

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -24,4 +24,18 @@ describe('growUp', () => {
 
         expect(pet.age).toEqual(1);
     });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    })
+    it('increments the fitness by -3', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    })
 });
