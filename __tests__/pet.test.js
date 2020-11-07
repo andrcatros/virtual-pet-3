4205476a548ac5887dc9ce5223b64ede72e5015e
James Woodhead <james_woodhead95@hotmail.co.uk>
2020-11-07 10:38:29 +0000

    added functionality to feed pet to decrease hunger, using TDD to ensure feeding acted correctly and never dropped below a minimum hunger value. Re-factored to add minimum hunger constant

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -57,3 +57,21 @@ describe('walk', () => {
         expect(pet.fitness).toEqual(10);
     });
 });
+describe('feed', () => {
+    it('decreases hunger by 3', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 7;
+        pet.feed();
+
+        expect(pet.hunger).toEqual(4);
+    });
+    it('decreases hunger to a minimum of 0', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 2;
+        pet.feed();
+
+        expect(pet.hunger).toEqual(0);
+    });
+});
